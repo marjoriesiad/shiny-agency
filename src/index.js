378d83237d032091f2e569be4834e85b09430240
Marjorie <marjoriesiad@gmail.com>
2022-11-09 23:54:51 +0100

    styled all components and pages

--- src/index.js
+++ src/index.js
@@ -1,19 +1,34 @@
 import React from 'react'
-import {createRoot} from "react-dom/client"
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './pages/Home';
-import Survey from "./pages/Survey";
-import Header from "./components/Header";
-import Freelances from './pages/Freelances';
+import {createRoot} from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Freelances from './pages/Freelances'
+import Header from './components/Header'
+import Error from './components/Error'
+import { createGlobalStyle } from 'styled-components'
 
-const root = document.getElementById('root');
+const GlobalStyle = createGlobalStyle`
+    * {
+      font-family: 'Trebuchet MS', Helvetica, sans-serif;
+    }
+`
+
+const root = document.querySelector("#root");
+
 createRoot(root).render(
-  <Router>
-    <Header />
-    <Routes>
-      <Route exact path="/" element={<Home />} />  
-      <Route path="/survey" element={<Survey />} />
-      <Route path="/profils" element={<Freelances />} />  
-    </Routes>
-  </Router>
+    <Router>
+      <>
+      <GlobalStyle />
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />}/>
+        <Route path="/survey/:questionNumber" element={<Survey />}/>
+        <Route path="/freelances" element={<Freelances />}/>
+        <Route path="*" element={<Error />}/>
+      </Routes>
+      </>
+    </Router>
+
+
 )
